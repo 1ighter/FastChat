80788070329e4e985c0990185de3146eebfeb413
zhangjinpeng <zhangjinpeng01@bilibili.com>
2024-05-20 19:54:46 +0800

    fix

--- fastchat/serve/openai_api_server.py
+++ fastchat/serve/openai_api_server.py
@@ -818,7 +818,7 @@ async def sort_by_keyword_count(data, keywords):
 
 @app.post("/v2/chat/completions")
 async def create_chat_completion_v2(request: MyChatCompletionRequest):
-    model = "Qwen-7B-Chat"
+    model = "checkpoint-200"
     first_id = request.first_id
     second_id = request.second_id
     if second_id == "":
